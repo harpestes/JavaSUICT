c9af49c69c02262992df7132ac86e83456fbcd40
harpestes <dikaloandrij@gmail.com>
2023-11-27 15:40:01 +0200

    Reworked tests and solution logic

--- src/main/java/org/example/Palindrome.java
+++ src/main/java/org/example/Palindrome.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Palindrome {
     public boolean isPalindrome(String s) {
-        if(s.isEmpty() || s.length() == 1) return false;
-        return new StringBuilder(s).reverse().toString().equalsIgnoreCase(s);
+        if(s == null || s.length() == 1 || s.isEmpty()) return false;
+        return new StringBuilder(s).reverse().toString().replaceAll(" ", "").equalsIgnoreCase(s.replaceAll(" ", ""));
     }
 }
--- src/test/java/PalindromeTest.java
+++ src/test/java/PalindromeTest.java
@@ -12,25 +12,19 @@ public class PalindromeTest {
     }
 
     @Test
-    void testPalindromeTrue() {
-        assertTrue(palindrome.isPalindrome("racecar"));
-        assertTrue(palindrome.isPalindrome("raCecaR"));
-        assertTrue(palindrome.isPalindrome("redder"));
-        assertTrue(palindrome.isPalindrome("RedDer"));
-    }
-
-    @Test
-    void testPalindromeFalse() {
-        assertFalse(palindrome.isPalindrome("test"));
-    }
-
-    @Test
-    void testPalindromeEmpty() {
+    void isPalindrome() {
+        assertFalse(palindrome.isPalindrome(null));
         assertFalse(palindrome.isPalindrome(""));
-    }
-
-    @Test
-    void testPalindromeOneCharacter() {
         assertFalse(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome("radar"));
+        assertFalse(palindrome.isPalindrome("banana"));
+        assertTrue(palindrome.isPalindrome("hannah"));
+        assertTrue(palindrome.isPalindrome("pup"));
+        assertTrue(palindrome.isPalindrome("nan"));
+        assertFalse(palindrome.isPalindrome("lollipop"));
+        assertTrue(palindrome.isPalindrome("eye"));
+        assertTrue(palindrome.isPalindrome("6543456"));
+        assertTrue(palindrome.isPalindrome("step on no pets"));
+        assertTrue(palindrome.isPalindrome("A man a plan a canal Panama"));
     }
 }
